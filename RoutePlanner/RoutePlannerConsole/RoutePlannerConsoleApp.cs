c4e18844cf5ecf26b96f1a2f381072624bc89f45
nitesis <viviane.bendjus@students.fhnw.ch>
2015-02-18 15:22:29 +0100

    renamed namespace

--- RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RoutePlannerConsole
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
 {
     class RoutePlannerConsoleApp
     {
