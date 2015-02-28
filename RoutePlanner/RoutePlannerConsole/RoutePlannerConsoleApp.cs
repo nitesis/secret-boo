7eab43a815b6ff5b07ffb50ce55d508c8ad46d2d
nitesis <viviane.bendjus@students.fhnw.ch>
2015-02-28 13:55:26 +0100

    distance example
    added example for distance

--- RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -14,8 +14,12 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         {
             Console.WriteLine("Welcome to RoutePlanner {0}",
                     Assembly.GetExecutingAssembly().GetName().Version);
-            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
-            Console.WriteLine("{0}",wayPoint);
+            var wayPointWindisch = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            var wayPointBern = new WayPoint("Bern", 46.948811, 7.444480);
+            var wayPointTripolis = new WayPoint("Tripolis", 32.804241, 13.098579);
+            
+            Console.WriteLine("{0}", wayPointWindisch);
+            Console.WriteLine("{0} {1}", "Distance Bern - Tripolis: " ,wayPointBern.Distance(wayPointTripolis));
             Console.ReadKey();
         }
 
