d67db053fefe568010505eb16c4835bfd3e1abcc
SouzanH <susan.hw@hotmail.com>
2015-03-09 09:50:58 +0100

    quick fixes

--- RoutePlanner/RoutePlannerLib/RouteRequestEventArgs.cs
+++ RoutePlanner/RoutePlannerLib/RouteRequestEventArgs.cs
@@ -8,16 +8,16 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
     public class RouteRequestEventArgs: System.EventArgs 
     {
-        String fromCity { get; set; }
-        String toCity { get; set; }
-        TransportModes mode { set; get; }
+       public  String FromCity {get;set;}
+       public String ToCity { get; set; }
+       public TransportModes Mode { get; set; }
 
     
         public RouteRequestEventArgs(String from, String to , TransportModes mode)
         {
-            this.fromCity = from;
-            this.toCity = to;
-            this.mode = mode;
+            this.FromCity = from;
+            this.ToCity = to;
+            this.Mode = mode;
         }
     }
 }
