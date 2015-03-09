4ce19ac8af4b081db7f69a4e0a7de062cacf2897
nitesis <viviane.bendjus@students.fhnw.ch>
2015-03-09 10:52:15 +0100

    finished class RouteRequestWatcher

--- RoutePlanner/RoutePlannerLib/RouteRequestWatcher.cs
+++ RoutePlanner/RoutePlannerLib/RouteRequestWatcher.cs
@@ -11,10 +11,22 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
         public Dictionary<string, int> Log = new Dictionary<string, int>();
         public void LogRouteRequests(object sender, RouteRequestEventArgs e)
         {
-
+            if (Log.ContainsKey(e.ToCity))
+            {
+                Log[e.ToCity]++;
+            }
+            else
+            {
+                Log.Add(e.ToCity, 1);
+            }
+            
         }
-       public int GetCityRequests(string cityName)
+        public int GetCityRequests(string cityName)
         {
+            if (Log.ContainsKey(cityName))
+            {
+                return Log[cityName];
+            }
             return 0;
         }
 
