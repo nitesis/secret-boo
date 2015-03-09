3bec3408400c119230df08cce20f6f5129fe9c5a
SouzanH <susan.hw@hotmail.com>
2015-03-09 09:41:39 +0100

    Class RouteRequestWatcher added

---
+++ RoutePlanner/RoutePlannerLib/RouteRequestWatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    class RouteRequestWatcher
+    {
+        Dictionary<string, int> log = new Dictionary<string, int>();
+    }
+}
