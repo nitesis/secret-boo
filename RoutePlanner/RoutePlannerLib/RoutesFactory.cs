c9f4d8ed46ad4f202cf830cbbed286288280e68d
nitesis <viviane.bendjus@students.fhnw.ch>
2015-03-26 20:58:47 +0100

    init project lab5

---
+++ RoutePlanner/RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,24 @@
+﻿
+using RoutePlannerLib.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            return Create(cities, Settings.Default.RouteAlgorithm);
+        }
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            return null;
+        }
+    }
+}
