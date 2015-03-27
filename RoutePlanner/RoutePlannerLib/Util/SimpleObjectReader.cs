b1f49923dc1d6f7d8346004c77f2b248cef5edd3
SouzanH <susan.hw@hotmail.com>
2015-03-27 11:39:08 +0100

    Serializer Primitive data types  added

---
+++ RoutePlanner/RoutePlannerLib/Util/SimpleObjectReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+     public class SimpleObjectReader
+    {
+        
+        BinaryFormatter formatter =new BinaryFormatter();
+        TextReader stream;
+
+        public SimpleObjectReader(TextReader str)
+        {
+            this.stream = str;
+        }
+
+        public SimpleObjectReader()
+        {
+
+        }
+
+        public Object Next()
+        {
+            return stream.ReadLine();
+        }
+    }
+}
--- RoutePlanner/RoutePlannerLib/WayPoint.cs
+++ RoutePlanner/RoutePlannerLib/WayPoint.cs
@@ -7,6 +7,7 @@ using System.Data;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
+   [Serializable]
    public  class  WayPoint
     {
        
