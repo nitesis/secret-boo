f6ea39cd836b7220eeb7648476e275ca8bf4e42a
nitesis <viviane.bendjus@students.fhnw.ch>
2015-05-01 17:20:45 +0200

    added GC performance test

---
+++ RoutePlanner/GCTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// GC.collect: 151 ms, MemoryBefore: 168812, MemoryAfter: 105040
+            /// No GC.collect: 47 ms, MemoryBefore: 168812, MemoryAfter: 1005152
+            // the true argument tells the GC to perform a collection first
+            long memoryUsedBefore = GC.GetTotalMemory(false);
+            Stopwatch s = Stopwatch.StartNew();
+            generateObject(200);
+            GC.Collect();
+            s.Stop();
+            Console.WriteLine("Time elapsed: {0} milliseconds", s.ElapsedMilliseconds);
+            Console.WriteLine("Total Memory before: {0} and Total Memory after: {1}", memoryUsedBefore, GC.GetTotalMemory(false));
+            Console.ReadKey();
+            
+        }
+
+        public static void generateObject(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                // makes n 100MB objects
+                var temp = new Byte[100000];
+            }
+        }
+            
+    }
+}
--- RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using System.Diagnostics;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
 {
